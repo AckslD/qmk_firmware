22fe06e6b326eb691d57e2e337c496410d821a83
Axel Dahlberg <git@valleymnt.com>
2021-09-27 13:18:22 +0200

    remove animations to reduce size

--- keyboards/splitkb/kyria/keymaps/acksld/rules.mk
+++ keyboards/splitkb/kyria/keymaps/acksld/rules.mk
@@ -1,5 +1,6 @@
 BOOTLOADER = caterina      # Bootloader to use
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+OLED_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 # UNICODEMAP_ENABLE = yes    # Enable unicode_map
