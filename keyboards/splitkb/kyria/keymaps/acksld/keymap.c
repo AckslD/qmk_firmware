5c857267109d4a321415937a0577f4539e570bde
Axel Dahlberg <git@valleymnt.com>
2021-09-27 20:07:12 +0200

    fix oled

--- keyboards/splitkb/kyria/keymaps/acksld/keymap.c
+++ keyboards/splitkb/kyria/keymaps/acksld/keymap.c
@@ -260,7 +260,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* return state; */
 /* } */
 
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 	return OLED_ROTATION_180;
 }
